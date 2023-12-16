5615d88fef772805eb6c0e01d82b131c20a1d792
harlanhowe <harlan.howe@kinkaid.org>
2023-12-16 14:41:20 -0600

    Worked on GUI for top panel: separated Array & ArrayList popups and cosmetic changes.

--- src/CollectionDemoFrame.java
+++ src/CollectionDemoFrame.java
@@ -1,4 +1,6 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -9,30 +11,29 @@ public class CollectionDemoFrame extends JFrame implements ActionListener
 {
 
     private CollectionDemoPanel mainPanel;
-    private JComboBox commandsComboBox;
-    private JButton executeButton;
+    private JComboBox arrayCommandsComboBox, ALCommandsComboBox;
+    private JButton executeArrayButton, executeALButton;
     private JSpinner intSpinner;
     private HexItemPanel myHexPanel;
     private JButton newHexButton, nullHexButton;
 
     // these are the text lines for the popup menu.
-    private final String[] commands = {"Array: getHexAtIndex",
-                                 "Array: setHexAtIndex",
-                                 "Array: getNumHexes",
-                                "Array: removeAllHexes",
-                                "Array: insertHexAtLocation",
-                                "Array: removeHexAtLocation",
-                                "Array: removeHexAtLocation and shift down",
-                                "-------",
-                                "ArrayList: getHexAtIndex",
-                                "ArrayList: setHexAtIndex",
-                                "ArrayList: getNumHexes",
-                                "ArrayList: removeAllHexes",
-                                "ArrayList: addHexToEnd",
-                                "ArrayList: insertHexAtLocation",
-                                "ArrayList: removeHexAtLocation",
-                                "ArrayList: contains",
-                                "ArrayList: indexOf"};
+    private final String[] arrayCommands = {"getHexAtIndex",
+                                 "setHexAtIndex",
+                                 "getNumHexes",
+                                "removeAllHexes",
+                                "insertHexAtLocation",
+                                "removeHexAtLocation",
+                                "removeHexAtLocation & shift down"};
+    private final String[] arrayListCommands = {"getHexAtIndex",
+                                "setHexAtIndex",
+                                "getNumHexes",
+                                "removeAllHexes",
+                                "addHexToEnd",
+                                "insertHexAtLocation",
+                                "removeHexAtLocation",
+                                "contains",
+                                "indexOf"};
 
     // a label at the bottom of the screen that displays any exceptions thrown.
     private JLabel exceptionLabel;
@@ -52,30 +53,9 @@ public class CollectionDemoFrame extends JFrame implements ActionListener
         mainPanel = new CollectionDemoPanel();
 
         // Generate the Controls panel.
-        JPanel controlsPanel = new JPanel();
-        controlsPanel.setLayout(new FlowLayout());
-
-        commandsComboBox = new JComboBox(commands);
-        controlsPanel.add(commandsComboBox);
-
-        executeButton = new JButton("Execute.");
-        executeButton.addActionListener(this); // if somebody presses the button, call this.actionPeformed().
-        controlsPanel.add(executeButton);
-
-        intSpinner = new JSpinner(new SpinnerNumberModel(0,-1,100,1));
-        controlsPanel.add(intSpinner);
-
-        myHexPanel = new HexItemPanel();
-        myHexPanel.setMyItem(new HexItem());
-        controlsPanel.add(myHexPanel);
-
-        newHexButton = new JButton ("New Hex");
-        newHexButton.addActionListener(this); // if somebody presses the button, call this.actionPeformed().
-        controlsPanel.add(newHexButton);
-
-        nullHexButton = new JButton ("Null Hex");
-        nullHexButton.addActionListener(this); // if somebody presses the button, call this.actionPeformed().
-        controlsPanel.add(nullHexButton);
+        Box controlsPanel = Box.createHorizontalBox();
+        makeCommandsBox(controlsPanel);
+        makeIOBox(controlsPanel);
 
         // Generate the exception Label
         exceptionLabel = new JLabel("");
@@ -85,6 +65,74 @@ public class CollectionDemoFrame extends JFrame implements ActionListener
         getContentPane().add(controlsPanel, BorderLayout.NORTH);
         getContentPane().add(mainPanel, BorderLayout.CENTER);
         getContentPane().add(exceptionLabel, BorderLayout.SOUTH);
+    }
+
+    private void makeIOBox(Box controlsPanel)
+    {
+        Box ioBox = Box.createHorizontalBox();
+        ioBox.setBorder(new TitledBorder("input/output"));
+        controlsPanel.add(ioBox);
+
+        Box spinnerBox = Box.createVerticalBox();
+        spinnerBox.add(Box.createVerticalGlue());
+
+        intSpinner = new JSpinner(new SpinnerNumberModel(0,-1,100,1));
+//        intSpinner.setBorder(new TitledBorder("Int"));
+        spinnerBox.add(intSpinner);
+        spinnerBox.add(Box.createVerticalGlue());
+        ioBox.add(spinnerBox);
+
+        ioBox.add(Box.createHorizontalStrut(60));
+
+        Box hexBox = Box.createHorizontalBox();
+//        hexBox.setBorder(new TitledBorder("Hex"));
+        ioBox.add(hexBox);
+
+        Box hexPanelBox = Box.createVerticalBox();
+        hexPanelBox.add(Box.createVerticalGlue());
+        myHexPanel = new HexItemPanel();
+        myHexPanel.setMyItem(new HexItem());
+        hexPanelBox.add(myHexPanel);
+        hexPanelBox.add(Box.createVerticalGlue());
+        hexBox.add(hexPanelBox);
+
+        Box hexButtonsPanel = Box.createVerticalBox();
+        newHexButton = new JButton ("\uD83C\uDD95");
+        newHexButton.addActionListener(this); // if somebody presses the button, call this.actionPeformed().
+        hexButtonsPanel.add(newHexButton);
+
+        nullHexButton = new JButton ("∅");
+        nullHexButton.addActionListener(this); // if somebody presses the button, call this.actionPeformed().
+        hexButtonsPanel.add(nullHexButton);
+
+        hexBox.add(hexButtonsPanel);
+    }
+
+    private void makeCommandsBox(Box controlsPanel)
+    {
+        Box commandsBox = Box.createVerticalBox();
+
+        JPanel arraysCommandPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        JPanel arrayListCommandPanel = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+        commandsBox.add(arraysCommandPanel);
+        arraysCommandPanel.setBorder(new TitledBorder("Arrays"));
+        commandsBox.add(arrayListCommandPanel);
+        arrayListCommandPanel.setBorder(new TitledBorder("Array Lists"));
+        controlsPanel.add(commandsBox);
+
+        arrayCommandsComboBox = new JComboBox(arrayCommands);
+        arraysCommandPanel.add(arrayCommandsComboBox);
+
+        executeArrayButton = new JButton("Execute");
+        executeArrayButton.addActionListener(this); // if somebody presses the button, call this.actionPeformed().
+        arraysCommandPanel.add(executeArrayButton);
+
+        ALCommandsComboBox = new JComboBox(arrayListCommands);
+        arrayListCommandPanel.add(ALCommandsComboBox);
+
+        executeALButton = new JButton("Execute");
+        executeALButton.addActionListener(this); // if somebody presses the button, call this.actionPeformed().
+        arrayListCommandPanel.add(executeALButton);
     }
 
 
@@ -97,10 +145,15 @@ public class CollectionDemoFrame extends JFrame implements ActionListener
     @Override
     public void actionPerformed(ActionEvent e)
     {
-        if (e.getSource() == executeButton)
+        if (e.getSource() == executeArrayButton)
         {
-            executeCommand(commandsComboBox.getSelectedIndex()); // getSelectedIndex tells us the number of the row of
+            executeArrayCommand(arrayCommandsComboBox.getSelectedIndex()); // getSelectedIndex tells us the number of the row of
                                                                  //     the comboxbox that is showing.
+        }
+        if (e.getSource() == executeALButton)
+        {
+            executeALCommand(ALCommandsComboBox.getSelectedIndex()); // getSelectedIndex tells us the number of the row of
+            //     the comboxbox that is showing.
         }
         if (e.getSource() == newHexButton)
         {
@@ -112,7 +165,7 @@ public class CollectionDemoFrame extends JFrame implements ActionListener
         }
     }
 
-    public void executeCommand(int which)
+    public void executeArrayCommand(int which)
     {
         exceptionLabel.setText("");
         int num = (Integer)(intSpinner.getValue());
@@ -178,12 +231,72 @@ public class CollectionDemoFrame extends JFrame implements ActionListener
 
                     break;
                 case 16: //ArrayList indexOf
-                {
                     int idx = mainPanel.ALIndexOf(hex);
                     intSpinner.setValue(idx);
                     JOptionPane.showMessageDialog(this, "Method returned " + idx,
                             "AL - Index Of", JOptionPane.INFORMATION_MESSAGE);
-                }
+                    break;
+            }
+            // since the above method calls might have changed what
+            //   should have appeared on the screen, tell the main
+            //   panel that it needs to call paintComponent() at its
+            //   next opportunity.
+            mainPanel.repaint();
+        }
+        catch (Exception exp)
+        {
+            // instead of crashing, put the exception at the bottom of
+            // the screen...
+            exceptionLabel.setText(exp.toString());
+            JOptionPane.showMessageDialog(this,exp.toString(),"Error Returned",JOptionPane.WARNING_MESSAGE);
+        }
+
+    }
+
+    public void executeALCommand(int which)
+    {
+        exceptionLabel.setText("");
+        int num = (Integer)(intSpinner.getValue());
+        HexItem hex = myHexPanel.getMyItem();
+        int n;
+        try // because we might throw an exception doing this....
+        {
+            switch (which)
+            {
+                case 0: //ArrayList get hex at index
+                    myHexPanel.setMyItem(mainPanel.ALGetHexAtIndex(num));
+                    break;
+                case 1: //ArrayList set hex at index
+                    mainPanel.ALSetHexAtIndex(hex,num);
+                    break;
+                case 2: //ArrayList get numHexes
+                    n = mainPanel.ALGetNumHexes();
+                    intSpinner.setValue(n);
+                    JOptionPane.showMessageDialog(this,"Method returned "+n,
+                            "AL - GetNumHexes",JOptionPane.INFORMATION_MESSAGE);
+                    break;
+                case 3: //ArrayList removeAllHexes
+                    mainPanel.ALRemoveAllHexes();
+                    break;
+                case 4: //ArrayList addHexToEnd
+                    mainPanel.ALAddHexToEnd(hex);
+                    break;
+                case 5: //ArrayList insertHexAtLocation
+                    mainPanel.ALInsertHexAtLocation(hex,num);
+                    break;
+                case 6: //ArrayList removeHexAtLocation
+                    mainPanel.ALRemoveHexAtLocation(num);
+                    break;
+                case 7: //ArrayList contains
+                    JOptionPane.showMessageDialog(this, mainPanel.ALContains(hex), "AL - Contains", JOptionPane.INFORMATION_MESSAGE);
+
+                    break;
+                case 8: //ArrayList indexOf
+                    int idx = mainPanel.ALIndexOf(hex);
+                    intSpinner.setValue(idx);
+                    JOptionPane.showMessageDialog(this, "Method returned " + idx,
+                            "AL - Index Of", JOptionPane.INFORMATION_MESSAGE);
+                    break;
             }
             // since the above method calls might have changed what
             //   should have appeared on the screen, tell the main
